2d8525a49264fed512ec9ee85e46f72a9c8d919b
Junhui <52191549+Hui-of-limin@users.noreply.github.com>
2020-03-01 22:43:57 -0500

    set_tables modify.

--- database/set_tables.sql
+++ database/set_tables.sql
@@ -45,3 +45,25 @@ low FLOAT,
 PRIMARY KEY(stock_ID, minute),
 FOREIGN KEY(stock_ID) REFERENCES stock_name(stock_ID)
 )
+
+
+#change some mistakes
+
+
+alter table history_hour change hign high int;
+
+alter table history_day change hign high int;
+
+alter table real_time change hign high int;
+
+#change timestamp to int;#change timestamp to int
+
+alter table real_time modify minute int;
+
+alter table history_day modify day int;
+
+alter table history_hour modify hour int;
+
+
+
+
